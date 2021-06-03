619cfc3f49de97f22e6d4a4512341a694b63c6f9
王立发 <wanglf@weipaitang.com>
2021-06-03 20:00:40 +0800

    feat: 第三个图表完成

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,20 +2,28 @@ import React, {useEffect, useRef} from 'react';
 import './home.scss';
 import headerBg from '../images/header.png';
 import {Chart1} from '../components/chart1';
+import {Chart2} from '../components/chart2';
+import {Chart3} from '../components/chart3';
 export const Home = () => {
-  
+  const year = new Date().getFullYear();
   return (
     <div className="home">
       <header style={{backgroundImage: `url(${headerBg})`}}/>
       <main>
         <section className="section1">
-          <Chart1 />
+          <Chart1/>
+          <Chart2/>
         </section>
-        <section className="bordered section2"></section>
+        <section className="bordered section2">
+          <Chart3 />
+        </section>
         <section className="bordered section3"></section>
         <section className="bordered section4"></section>
         <section className="bordered section5"></section>
       </main>
+      <footer>
+        &copy; 杭州市 2020-{year}
+      </footer>
     </div>
   );
 };
