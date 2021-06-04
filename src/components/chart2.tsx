a6d9909738e8261881120191a779353b66d563d2
王立发 <wanglf@weipaitang.com>
2021-06-04 17:50:25 +0800

    feat: 添加动态数据

--- src/components/chart2.tsx
+++ src/components/chart2.tsx
@@ -1,65 +1,81 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import * as echarts from "echarts";
 import {createEchartsOptions} from '../shared/create-echarts-options';
 
 export const Chart2 = () => {
   const divRef = useRef(null);
+  const myChart = useRef(null);
+  const [data, setData] = useState([]);
   useEffect(() => {
-    var myChart = echarts.init(divRef.current);
-    myChart.setOption(createEchartsOptions({
-      xAxis: {
-        type: 'value',
-        boundaryGap: [0, 0.01],
-        splitLine: {show: false},
-        axisLabel: {show: false}
-      },
-      yAxis: {
-        axisTick: {show: false},
-        type: 'category',
-        data: ['城关区公安局', '七里河区公安局', '西固区公安局', '安宁区公安局', '红古区公安局',
-          '永登县公安局', '皋兰县公安局', '榆中县公安局', '新区公安局'],
-        axisLabel: {
-          formatter(val) {
-            return val.replace('公安局', '\n公安局');
-          }
-        }
-      },
-      series: [
-        {
-          name: '2011年',
-          type: 'bar',
-          data: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-          itemStyle: {
-            normal: {
-              color: new echarts.graphic.LinearGradient(0, 0, 1, 0, [{
-                offset: 0,
-                color: '#2034f9'
-              }, {
-                offset: 1,
-                color: '#04a1ff'
-              }])
-            }
-          }
-        },
-        {
-          name: '2012年',
-          type: 'bar',
-          data: [2, 3, 4, 5, 6, 7, 8, 9, 10],
-          itemStyle: {
-            normal: {
-              color: new echarts.graphic.LinearGradient(0, 0, 1, 0, [{
-                offset: 0,
-                color: '#b92ae8'
-              }, {
-                offset: 1,
-                color: '#6773e7'
-              }])
-            }
-          }
-        }
-      ]
-    }));
+    myChart.current = echarts.init(divRef.current);
+    const timer = setInterval(() => {
+      setData([
+        {name: '城关区公安局', 2011: 2, 2012: Math.random() * 10},
+        {name: '七里河区公安局', 2011: 2, 2012: 3},
+        {name: '西固区公安局', 2011: 2, 2012: 3},
+        {name: '安宁区公安局', 2011: 2, 2012: 3},
+        {name: '红古区公安局', 2011: 2, 2012: 3},
+        {name: '永登县公安局', 2011: 2, 2012: 3},
+        {name: '皋兰县公安局', 2011: 2, 2012: 3},
+        {name: '榆中县公安局', 2011: 2, 2012: 3},
+        {name: '新区公安局', 2011: 2, 2012: 3},
+      ])
+    }, 1000)
+    return () => clearInterval(timer);
   }, []);
+
+  useEffect(() => myChart.current.setOption(createEchartsOptions({
+    xAxis: {
+      type: 'value',
+      boundaryGap: [0, 0.01],
+      splitLine: {show: false},
+      axisLabel: {show: false}
+    },
+    yAxis: {
+      axisTick: {show: false},
+      type: 'category',
+      data: data.map(i => i.name),
+      axisLabel: {
+        formatter(val) {
+          return val.replace('公安局', '\n公安局');
+        }
+      }
+    },
+    series: [
+      {
+        name: '2011年',
+        type: 'bar',
+        data: data.map(i => i[2011]),
+        itemStyle: {
+          normal: {
+            color: new echarts.graphic.LinearGradient(0, 0, 1, 0, [{
+              offset: 0,
+              color: '#2034f9'
+            }, {
+              offset: 1,
+              color: '#04a1ff'
+            }])
+          }
+        }
+      },
+      {
+        name: '2012年',
+        type: 'bar',
+        data: data.map(i => i[2012]),
+        itemStyle: {
+          normal: {
+            color: new echarts.graphic.LinearGradient(0, 0, 1, 0, [{
+              offset: 0,
+              color: '#b92ae8'
+            }, {
+              offset: 1,
+              color: '#6773e7'
+            }])
+          }
+        }
+      }
+    ]
+  })), [data])
 
   return (
     <div className="bordered 破获排名">
