9f52dc19ac99c1fe8c04f8eb29057ab8b7b47ee4
王立发 <wanglf@weipaitang.com>
2021-06-04 17:58:56 +0800

    feat: 完成全部图表

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -10,6 +10,11 @@ import {Chart6} from '../components/chart6';
 import {Chart7} from '../components/chart7';
 import {Chart8} from '../components/chart8';
 import {Chart9} from '../components/chart9';
+import {Chart10} from '../components/chart10';
+import {Chart11} from '../components/chart11';
+import {Chart12} from '../components/chart12';
+import {Chart13} from '../components/chart13';
+import {Chart14} from '../components/chart14';
 export const Home = () => {
   const year = new Date().getFullYear();
   return (
@@ -20,9 +25,9 @@ export const Home = () => {
           <Chart1/>
           <Chart2/>
         </section>
-        <section className="bordered section2">
-          <Chart3 />
-          <Chart4 />
+        <section className="section2">
+          <Chart3/>
+          <Chart4/>
         </section>
         <section className="bordered section3">
           <Chart5/>
@@ -38,10 +43,29 @@ export const Home = () => {
             </div>
           </div>
         </section>
-        <section className="bordered section5"></section>
+        <section className="section5">
+          <div className="bordered row1 案发类型">
+            <h2>案发类型统计</h2>
+            <div className="charts">
+              <Chart10/>
+              <Chart11/>
+            </div>
+          </div>
+          <div className="bordered row2 案发街道">
+            <h2>案发街道统计</h2>
+            <div className="charts">
+              <Chart12/>
+              <Chart13/>
+            </div>
+          </div>
+          <div className="bordered row3 作案手段">
+            <h2>作案手段分析</h2>
+            <Chart14/>
+          </div>
+        </section>
       </main>
       <footer>
-        &copy; 兰州市 2020-{year}
+        &copy; 兰州市 2021-{year}
       </footer>
     </div>
   );
