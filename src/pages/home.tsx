f080b164f080bd6bee357cf66c3cd986be203580
王立发 <wanglf@weipaitang.com>
2021-06-04 14:26:42 +0800

    feat: 完成 图表4

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,6 +4,7 @@ import headerBg from '../images/header.png';
 import {Chart1} from '../components/chart1';
 import {Chart2} from '../components/chart2';
 import {Chart3} from '../components/chart3';
+import {Chart4} from '../components/chart4';
 export const Home = () => {
   const year = new Date().getFullYear();
   return (
@@ -16,13 +17,14 @@ export const Home = () => {
         </section>
         <section className="bordered section2">
           <Chart3 />
+          <Chart4 />
         </section>
         <section className="bordered section3"></section>
         <section className="bordered section4"></section>
         <section className="bordered section5"></section>
       </main>
       <footer>
-        &copy; 杭州市 2020-{year}
+        &copy; 兰州市 2020-{year}
       </footer>
     </div>
   );
